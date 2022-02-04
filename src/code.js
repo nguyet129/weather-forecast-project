1fd69a89eb4366003fadff208f04aa2dc57d7cbe
nguyet129 <98861476+nguyet129@users.noreply.github.com>
2022-02-04 17:13:56 +0700

    Complete AQI and UVI cards
    Complete 90% current city report
    To do:  (1) Temperature degree buttons (temperature card) and (2) precipitation amount (precipitation card)

--- src/code.js
+++ src/code.js
@@ -67,29 +67,80 @@ fUnit.addEventListener("click", displayFTemp);
 
 //Feature 3: When a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.//
 
-//3.2. Retrieve and display city's name and current temperature from 3.1 API call and inject the result into HTML//
+//3.3. Retrieve and display relevant weather info from 3.2 API calls and inject the result into HTML//
+function displayCityAqi(response) {
+  console.log(response);
+  let retrievedAqi = response.data.list[0].main.aqi;
+  //let aqiIndexLookup = ["0", "0-25", "25-50", "50-75", "75-100", "100+"];
+  let aqi = document.querySelector("#aqi");
+  //aqi.innerHTML = aqiIndexLookup[retrievedAqi];
+  aqi.innerHTML = retrievedAqi;
+
+  let aqiLookup = ["None", "Good", "Fair", "Moderate", "Poor", "Very Poor"];
+  let retrievedAqiDesc = aqiLookup[retrievedAqi];
+  let aqiDesc = document.querySelector("#aqi-des");
+  aqiDesc.innerHTML = retrievedAqiDesc;
+
+  let aqiDetailLookup = [
+    "Ideal air quality for outdoor activities.",
+    "Ideal air quality for outdoor activities.",
+    "Only very few hypersensitive people should reduce outdoor activities.",
+    "No need to modify your usual outdoor activities unless you experience symptoms such as coughing and throat irritation.",
+    "Consider reducing or rescheduling strenuous activities outdoors if you experience symptoms such as coughing and throat irritation.",
+    "Reduce or reschedule strenuous activities outdoors, especially if you experience symptoms such as coughing and throat irritation.",
+  ];
+  let retrievedAqiDetailDesc = aqiDetailLookup[retrievedAqi];
+  let aqiDetailDesc = document.querySelector("#aqi-detailed-des");
+  aqiDetailDesc.innerHTML = retrievedAqiDetailDesc;
+}
+
 function displayCityWeather(response) {
   console.log(response);
   let retrievedWeatherDescription =
     response.data.current.weather[0].description;
-  let WeatherDescription = document.querySelector("#description");
-  WeatherDescription.innerHTML = retrievedWeatherDescription;
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = retrievedWeatherDescription;
 
   let retrievedPrecipProb = `${response.data.hourly[0].pop * 100}%`;
   console.log(typeof retrievedPrecipProb);
-  let PrecipProb = document.querySelector("#precip-prob");
-  PrecipProb.innerHTML = retrievedPrecipProb;
+  let precipProb = document.querySelector("#precip-prob");
+  precipProb.innerHTML = retrievedPrecipProb;
 
   //To revisit - if retrieved prob = 0, precipamt = 0, else display retrieved precipamt//
-  let PrecipAmt = document.querySelector("#precip-amt");
+  let precipAmt = document.querySelector("#precip-amt");
   if (retrievedPrecipProb === "0") {
-    PrecipAmt.innerHTML = `0`;
+    precipAmt.innerHTML = `0`;
   } else {
-    PrecipAmt.innerHTML = response.data.hourly[0].clouds;
+    precipAmt.innerHTML = response.data.hourly[0].clouds;
   }
+
+  let retrievedUvi = Math.round(response.data.current.uvi);
+  let uvIndexLookup = ["0", "0-2", "3-5", "6-7", "8-10", "11+"];
+  let uvi = document.querySelector("#uvi");
+  uvi.innerHTML = uvIndexLookup[retrievedUvi];
+
+  let uviLookup = ["None", "Low", "Moderate", "High", "Very High", "Extreme"];
+  let retrievedUviDesc = uviLookup[retrievedUvi];
+  let uviDesc = document.querySelector("#uvi-des");
+  uviDesc.innerHTML = retrievedUviDesc;
+
+  let uviDetailLookup = [
+    "Wear sunglasses on bright days. If you burn easily, cover up and use broad spectrum SPF 15+ sunscreen. Bright surfaces, sand, water, and snow will increase UV exposure.",
+    "Wear sunglasses on bright days. If you burn easily, cover up and use broad spectrum SPF 15+ sunscreen. Bright surfaces, sand, water, and snow will increase UV exposure.",
+    "Stay in shade near midday when the Sun is strongest. If outdoors, wear sun-protective clothing, a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 15+ sunscreen every 1.5 hours, even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water, and snow, will increase UV exposure.",
+    "Reduce time in the sun between 10 a.m. and 4 p.m. If outdoors, seek shade and wear sun-protective clothing, a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 15+ sunscreen every 1.5 hours, even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water, and snow, will increase UV exposure.",
+    "Minimize sun exposure between 10 a.m. and 4 p.m. If outdoors, seek shade and wear sun-protective clothing, a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 15+ sunscreen every 1.5 hours, even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water, and snow, will increase UV exposure.",
+    "Try to avoid sun exposure between 10 a.m. and 4 p.m. If outdoors, seek shade and wear sun-protective clothing, a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 15+ sunscreen every 1.5 hours, even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water, and snow, will increase UV exposure.",
+  ];
+  let retrievedUviDetailDesc = uviDetailLookup[retrievedUvi];
+  let uviDetailDesc = document.querySelector("#uvi-detailed-des");
+  uviDetailDesc.innerHTML = retrievedUviDetailDesc;
 }
 
-function displayCityName(response) {
+//3.2. Retrieve and display city's name and current temperature from 3.1 Weather API call. Inject the result into HTML.
+//Call Onecall and Air API using retrieved long lat from 3.1 Current Weather API call.
+
+function displayCityNameTemp(response) {
   console.log(response);
   let retrievedCity = response.data.name;
   let currentCity = document.querySelector("#current-place");
@@ -110,10 +161,15 @@ function displayCityName(response) {
   let cityLon = response.data.coord.lon;
   let cityLat = response.data.coord.lat;
 
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/onecall?";
-  let apiKey = "0bc8b420ecade609fc97283e2769e598";
-  let apiUrl = `${apiEndpoint}lon=${cityLon}&lat=${cityLat}&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayCityWeather);
+  let onecallApiEndpoint = "https://api.openweathermap.org/data/2.5/onecall?";
+  let onecallApiKey = "0bc8b420ecade609fc97283e2769e598";
+  let onecallApiUrl = `${onecallApiEndpoint}lon=${cityLon}&lat=${cityLat}&appid=${onecallApiKey}`;
+  axios.get(onecallApiUrl).then(displayCityWeather);
+
+  let aqiApiEndpoint = "http://api.openweathermap.org/data/2.5/air_pollution?";
+  let aqiApiKey = "0bc8b420ecade609fc97283e2769e598";
+  let aqiApiUrl = `${aqiApiEndpoint}lon=${cityLon}&lat=${cityLat}&appid=${aqiApiKey}`;
+  axios.get(aqiApiUrl).then(displayCityAqi);
 }
 
 //3.1. Call Weather API using input city name//
@@ -124,7 +180,7 @@ function retrieveCityInput(event) {
   let apiKey = "0bc8b420ecade609fc97283e2769e598";
   let unit = "metric";
   let apiUrl = `${apiEndpoint}q=${cityInput.value}&units=${unit}&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayCityName);
+  axios.get(apiUrl).then(displayCityNameTemp);
 }
 let searchButton = document.querySelector("form");
 searchButton.addEventListener("submit", retrieveCityInput);
@@ -132,7 +188,7 @@ searchButton.addEventListener("submit", retrieveCityInput);
 //https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=0bc8b420ecade609fc97283e2769e598
 //Feature 4: Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.//
 
-//4.2. Use retrieved long lat from 4.1 to call Weather API //
+//4.2. Call Weather API using retrieved long lat from 4.1//
 function retrieveLongLat(position) {
   console.log(position);
   let long = position.coords.longitude;
@@ -141,7 +197,7 @@ function retrieveLongLat(position) {
   let apiKey = "0bc8b420ecade609fc97283e2769e598";
   let unit = "metric";
   let apiUrl = `${apiEndpoint}lat=${lat}&lon=${long}&units=${unit}&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayCityName);
+  axios.get(apiUrl).then(displayCityNameTemp);
 }
 //4.1. Set up Current button to trigger retrieving user's long lat on click//
 function retrieveCurrentPosition(event) {
